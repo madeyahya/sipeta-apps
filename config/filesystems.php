172e15d6ffefb3484b3f605d5ba61a4f5867feaa
I Made Intan Yahya Kurniawan <madeyahya30@gmail.com>
2025-09-03 19:19:34 +0800

    Folder Vendor

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,12 +36,18 @@ return [
             'serve' => true,
             'throw' => false,
             'report' => false,
+            'public_uploads' => [
+                'driver' => 'local',
+                'root' => public_path('uploads'),
+                'url'  => env('APP_URL') . '/uploads',
+                'visibility' => 'public',
+            ],
         ],
 
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
